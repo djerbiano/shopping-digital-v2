9ece491c388710cacf1b70b5427c182f0a906c3b
djerbiano <saberghoudi2222@hotmail.fr>
2025-07-03 02:23:51 +0200

    create contact page

--- app/_Components/clientSide/LinkIconHeader.jsx
+++ app/_Components/clientSide/LinkIconHeader.jsx
@@ -1,5 +1,6 @@
 "use client";
 import styles from "../../page.module.css";
+import { useRouter } from "next/navigation";
 import { MdHeadsetMic } from "react-icons/md";
 import { BsFillCartCheckFill } from "react-icons/bs";
 import { MdFavorite } from "react-icons/md";
@@ -9,14 +10,24 @@ import { MenuHamburgerContext } from "../../context/menuHamburgerContext";
 
 export default function LinkIconHeader({ icons }) {
   const { isMenuOpen, setIsMenuOpen, isDesktop } = useContext(MenuHamburgerContext);
+  const router = useRouter();
 
   const handleMenuClick = () => {
     setIsMenuOpen(false);
   };
+
+  const handleNavigation = () => {};
   if (!isDesktop && !isMenuOpen) return null;
   return (
     <div className={styles.linkIconHeader} ref={icons}>
-      <button type="button" aria-label="Contactez-nous" onClick={handleMenuClick}>
+      <button
+        type="button"
+        aria-label="Contactez-nous"
+        onClick={() => {
+          handleMenuClick;
+          router.push("/contact");
+        }}
+      >
         <MdHeadsetMic aria-hidden="true" focusable="false" />
       </button>
 
---
+++ app/contact/page.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+export default function Contact() {
+  return (
+    <section className={styles.contact} aria-labelledby="contact">
+      <h2 id="contact">Nous contacter</h2>
+      <div className={styles.contactImageContainer}>
+        <Image
+          src="/carteDuMonde.webp"
+          alt="photo de la carte du monde en arrière plan"
+          className={styles.contactImage}
+          priority
+          fetchPriority="high"
+          fill
+        />
+      </div>
+      <div className={styles.contactFormContainer}>
+        <form>
+          <label htmlFor="nom">Nom:</label>
+          <input type="text" id="nom" required />
+
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" required />
+
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" rows="5" cols="30" required></textarea>
+
+          <button type="submit" aria-label="Envoyer le message">Envoyer</button>
+        </form>
+      </div>
+    </section>
+  );
+}
