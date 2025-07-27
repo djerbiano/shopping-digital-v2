040bfda22dea0a9a6f5d68be2a9244548d3f2ef2
djerbiano <saberghoudi2222@hotmail.fr>
2025-07-27 09:29:53 +0200

    setup backend folder structure and implement MongoDB connection

---
+++ _backend/config/db.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+let isConnected = false;
+
+async function connectToDb() {
+  if (isConnected) {
+    console.log("Connexion MongoDB déjà active");
+    return;
+  }
+
+  const uri = process.env.MONGODB_URI;
+  const dbName = process.env.DB_NAME;
+
+  if (!uri) throw new Error("MONGODB_URI manquant dans .env");
+  if (!dbName) throw new Error("DB_NAME manquant dans .env");
+
+  try {
+    await mongoose.connect(uri, {
+      dbName,
+    });
+
+    isConnected = true;
+    console.log(`Connecté à MongoDB : ${dbName}`);
+  } catch (err) {
+    console.error("Erreur de connexion à MongoDB :", err);
+    throw err;
+  }
+}
+
+module.exports = connectToDb;
